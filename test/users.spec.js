e55e76f0ab34512c7dd7870af98ed2686e0b1b04
Joel-An <rejoelve@gmail.com>
2018-12-11 13:01:43 +0900

    test: POST /users 회원가입에 성공한 경우 테스트 구현

--- test/users.spec.js
+++ test/users.spec.js
@@ -1,4 +1,39 @@
+const User = require('../models/user');
+
 describe('Users', () => {
+  describe('POST /users (회원가입)', () => {
+    beforeEach(done => {
+      clearCollection(User, done);
+    });
+
+    describe('성공하면', () => {
+      it('profile name을 받아온다.', done => {
+        chai
+          .request(server)
+          .post(API_URI + '/users')
+          .send({
+            userName: 'userName',
+            profileName: 'profileName',
+            email: 'email@gmail.com',
+            password: '123',
+            confirmPassword: '123',
+          })
+          .end((err, res) => {
+            res.should.have.status(201);
+            res.shoud.be.json;
+            res.body.should.have.property(profileName, 'profileName');
+            done();
+          });
+      });
+    });
+
+    describe.skip('실패하면', () => {
+      it(' message를 받아온다.', done => {
+        done();
+      });
+    });
+  });
+
   describe.skip('GET /users', () => {
     it('it should GET all users', done => {
       chai
