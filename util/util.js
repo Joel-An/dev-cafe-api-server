3ea88fa9ac734ea194b7f0bc5291702686c3be72
Joel-An <rejoelve@gmail.com>
2018-12-11 20:15:59 +0900

    feat: 비동기 에러를 처리하기위한 wrapper 함수 추가
    대부분의 api 기능이 DB를 사용하므로
    모든 구현에 try/catch를 추가하기 보다는 next를 이용하여
    한꺼번에 오류처리기에서 처리한다

---
+++ util/util.js
@@ -0,0 +1,7 @@
+exports.wrapAsync = function(fn) {
+  return function(req, res, next) {
+    // async 함수fn 내부에서 발생한 모든 에러들을
+    // next()를 통해 오류처리기로 전달
+    fn(req, res, next).catch(next);
+  };
+};
