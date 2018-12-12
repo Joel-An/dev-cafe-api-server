c3fb5c72657f074ae0751a38e7a9d9190b14f2e8
Joel-An <rejoelve@gmail.com>
2018-12-12 21:17:53 +0900

    refactor: POST /auth 에러메시지 별도 파일로 분리, 프로미스를 await으로 대체
    가독성 향상을 위함

--- constants/message.js
+++ constants/message.js
@@ -12,6 +12,14 @@ const USER = {
   },
 };
 
+const AUTH = {
+  ERROR: {
+    WRONG_USERNAME: '존재하지 않는 사용자이름/이메일입니다.',
+    WRONG_PASSWORD: '비밀번호가 일치하지 않습니다.',
+  },
+};
+
 module.exports = {
   USER,
+  AUTH,
 };
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,36 +1,36 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const JwtSecretKey = require('../config/config').JwtSecretKey;
+const AUTH_MESSAGE = require('../constants/message').AUTH;
 
 const wrapAsync = require('../util/util').wrapAsync;
 
 exports.login = wrapAsync(async (req, res) => {
-  User.findOne({ userName: req.body.userName }).then(user => {
-    if (!user) {
-      res.status(403);
-      return res.json({ message: '아이디/비밀번호가 일치하지 않습니다.' });
+  let user = await User.findOne({ userName: req.body.userName });
+  if (!user) {
+    res.status(403);
+    return res.json({ message: AUTH_MESSAGE.ERROR.WRONG_USERNAME });
+  }
+
+  if (!user.validPassword(req.body.password)) {
+    res.status(403);
+    return res.json({ message: AUTH_MESSAGE.ERROR.WRONG_USERNAME });
+  }
+
+  const payload = {
+    _id: user._id,
+    email: user.email,
+  };
+  const secretOrPrivateKey = JwtSecretKey;
+  const options = { expiresIn: 60 * 60 * 24 };
+
+  jwt.sign(payload, secretOrPrivateKey, options, (err, token) => {
+    if (err) {
+      throw err;
     }
 
-    if (!user.validPassword(req.body.password)) {
-      res.status(403);
-      return res.json({ message: '비밀번호가 일치하지 않습니다.' });
-    }
+    const result = { accessToken: token };
 
-    const payload = {
-      _id: user._id,
-      email: user.email,
-    };
-    const secretOrPrivateKey = JwtSecretKey;
-    const options = { expiresIn: 60 * 60 * 24 };
-
-    jwt.sign(payload, secretOrPrivateKey, options, (err, token) => {
-      if (err) {
-        throw err;
-      }
-
-      const result = { accessToken: token };
-
-      res.json(result);
-    });
+    res.json(result);
   });
 });
