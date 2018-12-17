6a452b9074957af6f13d9aca384c9014df35c558
Joel-An <rejoelve@gmail.com>
2018-12-17 22:26:36 +0900

    docs: swagger 샘플 docs 제거

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,29 +1,3 @@
-/**
- * @swagger
- *
- * /auth:
- *   post:
- *     summary: Login
- *     tags:
- *       - AUTH
- *     description: Login to the application
- *     produces:
- *       - application/json
- *     parameters:
- *       - name: userName
- *         description: Username to use for login.
- *         in: formData
- *         required: true
- *         type: string
- *       - name: password
- *         description: User's password.
- *         in: formData
- *         required: true
- *         type: string
- *     responses:
- *       200:
- *         description: login
- */
 const apiRouterV1 = require('express').Router();
 
 const authRouter = require('./auth');
