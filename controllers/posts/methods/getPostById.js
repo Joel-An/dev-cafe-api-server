89c133e72b34918be9fa342a20fbbde1a3818b20
Joel-An <rejoelve@gmail.com>
2018-12-20 19:05:14 +0900

    feat: post  조회 시 카테고리,작성자 정보도 포함해서 응답

--- controllers/posts/methods/getPostById.js
+++ controllers/posts/methods/getPostById.js
@@ -12,7 +12,12 @@ module.exports = wrapAsync(async (req, res) => {
     return res.json({ message: 'postId 형식이 잘못되었습니다.' });
   }
 
-  const post = await Post.findById(postId);
+  const post = await Post.findById(postId)
+    .populate('author', 'profileName')
+    .populate({
+      path: 'category',
+      populate: { path: 'parentId' },
+    });
 
   if (!post) {
     return res.status(404).end();
