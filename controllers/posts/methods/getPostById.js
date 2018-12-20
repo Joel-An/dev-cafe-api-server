4d20bb5184738f1fe34ff344509251f3afa62d24
Joel-An <rejoelve@gmail.com>
2018-12-20 15:52:35 +0900

    feat: GET /posts/:id 구현

--- controllers/posts/methods/getPostById.js
+++ controllers/posts/methods/getPostById.js
@@ -1,10 +1,23 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Types;
+const { wrapAsync } = require('../../../util/util');
 const Post = require('../../../models/post');
 
-module.exports = (req, res) => {
-  Post.findById(req.params.id)
-    .lean()
-    .then(user => res.json(user))
-    .catch((err) => {
-      throw err;
-    });
-};
+module.exports = wrapAsync(async (req, res) => {
+  const postId = req.params.id;
+
+  if (!ObjectId.isValid(postId)) {
+    res.status(400);
+    return res.json({ message: 'postId 형식이 잘못되었습니다.' });
+  }
+
+  const post = await Post.findById(postId);
+
+  if (!post) {
+    return res.status(404).end();
+  }
+
+  res.status(200);
+  return res.json({ post });
+});
