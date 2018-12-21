d4fb9cab7feb8fa925b68a776abaf89508627a1b
Joel-An <rejoelve@gmail.com>
2018-12-21 14:19:04 +0900

    feat: GET /comments 구현

--- controllers/comments/methods/getComments.js
+++ controllers/comments/methods/getComments.js
@@ -1,4 +1,49 @@
-module.exports = (req, res) => {
-  res.status(200);
-  res.end();
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Types;
+const { wrapAsync } = require('../../../util/util');
+const Comment = require('../../../models/comment');
+
+const isValidQueryParam = (query, options) => {
+  let flag = true;
+  Object.keys(query).forEach((param) => {
+    if (!(param in options)) {
+      flag = false;
+    }
+  });
+  return flag;
 };
+
+const removeEmptyOption = (options) => {
+  const cleanedOptions = { ...options };
+  Object.keys(cleanedOptions).forEach((key) => {
+    if (!cleanedOptions[key]) { delete cleanedOptions[key]; }
+  });
+  return cleanedOptions;
+};
+
+module.exports = wrapAsync(async (req, res) => {
+  const { query } = req;
+  const options = { post: query.post };
+
+  if (!isValidQueryParam(query, options)) {
+    res.status(400);
+    return res.json({ message: '허용하지않는 쿼리 파라메터입니다.' });
+  }
+
+  if (options.post && !ObjectId.isValid(options.post)) {
+    res.status(400);
+    return res.json({ message: 'postId 형식이 틀렸습니다.' });
+  }
+
+  const cleanedOptions = removeEmptyOption(options);
+
+  const comments = await Comment.find(cleanedOptions);
+
+  if (comments.length === 0) {
+    return res.status(404).end();
+  }
+
+  res.status(200);
+  return res.json({ comments });
+});
