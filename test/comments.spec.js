d15e0e6141ebac29dd74cb7342565425a85a8314
Joel-An <rejoelve@gmail.com>
2018-12-21 14:18:46 +0900

    test: GET /comments 테스트 작성
    쿼리 파라메터로 post를 지정해서 해당 post에 달린 댓글만 가져올수 있음

--- test/comments.spec.js
+++ test/comments.spec.js
@@ -6,6 +6,13 @@ describe('comments', () => {
   let token;
   const user = copyAndFreeze(USER_ARRAY[0]);
   const testComment = copyAndFreeze(sampleComment);
+  const testCommentForPost2 = copyAndFreeze(sampleComment);
+
+  const reqGetComments = (query) => {
+    const queryString = query || '';
+    return requester
+      .get(`${API_URI}/comments?${queryString}`);
+  };
 
   before((done) => {
     dropDatabase(done);
@@ -32,12 +39,21 @@ describe('comments', () => {
     childC.should.have.status(201);
 
 
-    // 글 작성
+    // 글 2개 작성
     const testPost = copyAndFreeze(samplePost);
+    const testPost2 = copyAndFreeze(samplePost);
     testPost.categoryId = childC.body.categoryId;
-    const post = await reqPostPosts(token, testPost);
-    post.should.have.status(201);
-    testComment.postId = post.body.postId;
+    testPost2.categoryId = childC.body.categoryId;
+
+    const reqPost = reqPostPosts(token, testPost);
+    const reqPost2 = reqPostPosts(token, testPost2);
+
+    const responses = await Promise.all([reqPost, reqPost2]);
+    responses[0].should.have.status(201);
+    responses[1].should.have.status(201);
+
+    testComment.postId = responses[0].body.postId;
+    testCommentForPost2.postId = responses[1].body.postId;
   });
 
   after((done) => {
@@ -105,6 +121,79 @@ describe('comments', () => {
 
       const comments = await Comment.find({});
       comments.length.should.be.equal(0);
+    });
+  });
+
+  describe('GET /comments', () => {
+    let commentId1;
+    let commentId2;
+
+    after((done) => {
+      clearCollection(Comment, done);
+    });
+    context('댓글이 없으면', () => {
+      it('404코드를 반환한다', async () => {
+        const res = await reqGetComments();
+        res.should.have.status(404);
+      });
+    });
+
+    context('댓글이 있으면', () => {
+      before(async () => {
+        const req1 = reqPostComments(token, testComment);
+        const req2 = reqPostComments(token, testCommentForPost2);
+
+        const responses = await Promise.all([req1, req2]);
+        responses[0].should.have.status(201);
+        responses[1].should.have.status(201);
+
+        responses[0].body.should.have.property('commentId');
+        responses[1].body.should.have.property('commentId');
+
+        commentId1 = responses[0].body.commentId;
+        commentId2 = responses[1].body.commentId;
+      });
+
+      it('200코드, 전체 댓글을 반환한다', async () => {
+        const res = await reqGetComments();
+        res.should.have.status(200);
+
+        res.body.should.have.property('comments');
+
+        const { comments } = res.body;
+        assert.equal(comments.length, 2);
+        assert.equal(comments[0]._id, commentId1);
+        assert.equal(comments[1]._id, commentId2);
+      });
+
+      context('post(Id)를 쿼리스트링으로 지정하면', () => {
+        it('200코드, 해당되는 comments를 반환한다', async () => {
+          const query = `post=${testComment.postId}`;
+          const res = await reqGetComments(query);
+          res.should.have.status(200);
+          res.body.should.have.property('comments');
+
+          const { comments } = res.body;
+          assert.equal(comments.length, 1);
+          assert.equal(comments[0]._id, commentId1);
+        });
+      });
+      context('post(Id)가 invalid하면', () => {
+        it('400코드를 반환한다', async () => {
+          const query = 'post=INVALID_POSTID';
+          const res = await reqGetComments(query);
+          res.should.have.status(400);
+          res.body.should.not.have.property('comments');
+        });
+      });
+      context('허용하지않는 쿼리 파라메터를 사용하면', () => {
+        it('400코드를 반환한다', async () => {
+          const wrongQuery = 'pAst=22&isAdmin=true';
+          const res = await reqGetComments(wrongQuery);
+          res.should.have.status(400);
+          res.body.should.not.have.property('comments');
+        });
+      });
     });
   });
 });
