b2caf71f184f71a5a92f3a2fd120e9e9c2a57f65
Joel-An <rejoelve@gmail.com>
2019-01-03 17:23:12 +0900

    feat: 사용자 id도 전달하게 변경
    내가 쓴 글,댓글인지 클라이언트에서 확인할 방법이 필요해서 변경함

--- controllers/users/methods/getMyInfo.js
+++ controllers/users/methods/getMyInfo.js
@@ -1,5 +1,5 @@
 
 module.exports = (req, res) => {
-  const myInfo = { profileName: req.user.profileName };
+  const myInfo = { profileName: req.user.profileName, _id: req.user._id };
   res.status(200).json({ myInfo });
 };
