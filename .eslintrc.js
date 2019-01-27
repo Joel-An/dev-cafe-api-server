00849de132fbff0aea333fc454e01eddb51f3ba9
Joel-An <rejoelve@gmail.com>
2019-01-27 16:52:45 +0900

    chore: eslint max_len = 100

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'max-len': [
       'error',
       {
-        code: 80,
+        code: 100,
         ignoreUrls: true,
         ignoreRegExpLiterals: true,
         ignoreStrings: true,
