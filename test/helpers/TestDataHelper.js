c957ac5bd246428205e638202fca8cd493a48c3f
Joel-An <rejoelve@gmail.com>
2019-01-30 15:07:42 +0900

    feat: 글 작성 헬퍼 함수 추가
    함수형 프로그래밍 함수 go() 를 이용해서 여러개의 테스트 글 작성을 도와주는 함수

--- test/helpers/TestDataHelper.js
+++ test/helpers/TestDataHelper.js
@@ -3,6 +3,24 @@ const { curry } = require('fxjs2');
 
 const App = require('./App');
 
+const postPost = async (userToken, post) => {
+  const res = await App.reqPostPost(userToken, post);
+  const id = res.body.postId;
+  post.setId(id);
+
+  return post;
+};
+
+const createPostInto = (category, num = 0) => {
+  const post = new TestPost({
+    title: `test post${num} in ${category.name} category`,
+    contents: `test post${num}`,
+    categoryId: category._id,
+  });
+
+  return post;
+};
+
 const postComment = async (userToken, comment) => {
   const res = await App.reqPostComment(userToken, comment);
   const id = res.body.commentId;
@@ -30,12 +48,13 @@ const createChildCommentOf = (comment, num = 0) => {
   return childComment;
 };
 
+
 const TestDataHelper = {
+  postPost: curry(postPost),
+  createPostInto: curry(createPostInto),
   postComment: curry(postComment),
   createCommentInto: curry(createCommentInto),
   createChildCommentOf: curry(createChildCommentOf),
-
-
 };
 
 
