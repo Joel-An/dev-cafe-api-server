d43797cb933e0268f769301a61f841f5a27be171
Joel-An <rejoelve@gmail.com>
2019-02-16 10:56:33 +0900

    feat: getMyinfo isAdmin 정보 추가

--- controllers/users/methods/getMyInfo.js
+++ controllers/users/methods/getMyInfo.js
@@ -1,5 +1,9 @@
 
 module.exports = (req, res) => {
-  const myInfo = { profileName: req.user.profileName, _id: req.user._id };
+  const myInfo = {
+    profileName: req.user.profileName,
+    _id: req.user._id,
+    isAdmin: req.user.isAdmin,
+  };
   res.status(200).json({ myInfo });
 };
