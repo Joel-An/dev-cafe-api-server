e6124abc954c5f8e67e05976d137b2a521a6b78c
Joel-An <rejoelve@gmail.com>
2019-03-04 15:28:23 +0900

    feat: api docs 라우팅 변경
    nginx에서 /api를 기준으로 프록시 하도록 설정하기 위해서

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/api/v1', apiRouterV1);
 
 // catch 404 and forward to error handler
