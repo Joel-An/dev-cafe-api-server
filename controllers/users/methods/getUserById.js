59e0f2a0d34d5d18024ed3c7aceecb0e66b2c46d
Joel-An <rejoelve@gmail.com>
2019-03-04 15:27:46 +0900

    fix: /users 비밀번호, 이메일이 노출되는 현상 수정
    사용하지 않는 엔드포인트라 관리를 안 하고 있었음
    
    나중에 제거하더라도 일단은 비밀번호, 이메일은 보내지 않도록 바꿈

--- controllers/users/methods/getUserById.js
+++ controllers/users/methods/getUserById.js
@@ -1,7 +1,7 @@
 const User = require('../../../models/user');
 
 module.exports = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.id, '-password -email')
     .lean()
     .then((user) => {
       res.setHeader('Content-Type', 'application/json');
--- controllers/users/methods/getUsers.js
+++ controllers/users/methods/getUsers.js
@@ -1,7 +1,7 @@
 const User = require('../../../models/user');
 
 module.exports = (req, res) => {
-  User.find({})
+  User.find({}, '-password -email')
     .lean()
     .then((users) => {
       res.setHeader('Content-Type', 'application/json');
