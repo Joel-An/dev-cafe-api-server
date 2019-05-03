4a57186202192283b81724a77e82c7797d2418ed
Joel-An <rejoelve@gmail.com>
2019-05-03 20:03:08 +0900

    feat: 알림을 등록하고 소켓으로 전파하는 notifier 구현
    "A 유저가 B유저의 댓글에 좋아요를 누른다."
    
    A유저가 좋아요를 생성하는 http request를 보낸 것인데
    B유저에게 전달될 알림을 저장하고, 알림이 도착하는 걸 기다렸다가 응답할 필요는 없다.
    
    따라서 A 유저의 좋아요 요청을 처리하다가 분기(fork)할 필요가 있어서 알림을 처리하는 async 함수를 만들었다.
    
    async함수를 호출하면 await을 만나기 전까지 진행하다가 await 이후의 코드는 잡큐에 전달하고,
    제어를 caller에게 다시 전달한다.
    
    따라서 A유저의 http request는 바로 종료되고, 알림을 전달하는 로직은 jop queue에서 대기하다가 별도로 처리된다.
    
    당장은 알림이 실패했을 때 복구 전략이 없으므로 간단하게 log만 찍는 exception catcher만 만들어서 적용해두었다.

---
+++ util/notifier.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+
+const { ObjectId } = mongoose.Types;
+
+const Notification = require('../models/notification');
+const { userProjection } = require('../models/projectionFilters');
+
+const Socket = require('./Socket');
+
+const asyncExceptionCatcher = asyncFunc => (...args) => {
+  asyncFunc(...args).catch(err => console.error(err));
+};
+
+const areEqualObjectIds = (oid1, oid2) => {
+  const objectId1 = typeof oid1 === 'string' ? new ObjectId(oid1) : oid1;
+  const objectId2 = typeof oid1 === 'string' ? new ObjectId(oid2) : oid2;
+
+  return objectId1.equals(objectId2);
+};
+
+const populateNotification = notif => notif.populate('post', 'title')
+  .populate('comment', 'contents')
+  .populate('childComment')
+  .populate('fromWhom', userProjection)
+  .execPopulate();
+
+const notify = async (notification) => {
+  const populatedNotif = await populateNotification(notification);
+
+  Socket.emitNewNotification(populatedNotif.userId, populatedNotif);
+};
+
+exports.sendCommentLikesNotification = asyncExceptionCatcher(
+  async (fromUserId, toUserId, commentId, postId) => {
+    if (areEqualObjectIds(fromUserId, toUserId)) {
+      return;
+    }
+
+    const notification = new Notification({
+      fromWhom: fromUserId,
+      userId: toUserId,
+      comment: commentId,
+      post: postId,
+    }).commentLikes();
+
+    await notify(await notification.save());
+  }
+);
+
+exports.sendAuthorHeartNotification = asyncExceptionCatcher(
+  async (fromUserId, toUserId, commentId, postId) => {
+    if (areEqualObjectIds(fromUserId, toUserId)) {
+      return;
+    }
+
+    const notification = new Notification({
+      fromWhom: fromUserId,
+      userId: toUserId,
+      comment: commentId,
+      post: postId,
+    }).authorHeart();
+
+    await notify(await notification.save());
+  }
+);
